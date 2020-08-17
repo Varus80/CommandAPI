ef77978c2fcb3fcf6796f6ed45d6e95f54e31d8a
Varus80 <fer212010@gmail.com>
2020-08-16 23:40:22 -0400

    Added Command Model to API Project

---
+++ src/CommandAPI/Models/Command.cs
@@ -0,0 +1,10 @@
+namespace CommandAPI.Models
+{
+    public class CommandAPI
+    {
+        public int Id {get; set;}
+        public string HowTo {get; set;}
+        public string platform {get; set;}
+        public string CommandLine {get; set;}
+    }
+}
